51cbfe3e3cf9020a28f67046ccf06575c0acb279
DraftyHat <draftyhat@darken.cybertwilight.org>
2021-12-09 20:29:11 -0500

    day 9 part 2

--- 2021_09.c
+++ 2021_09.c
@@ -13,6 +13,9 @@
 #define AOC_DAY 9
 #define AOC_YEAR 2021
 
+/* value indicating grid square has been counted. Must be greater than 9. */
+#define COUNTED 10
+
 
 struct grid {
     int height;
@@ -23,6 +26,11 @@ struct grid {
 int grid_get_value(const struct grid * g, int x, int y)
 {
     return g->values[y][x];
+}
+
+void grid_set_value(struct grid * g, int x, int y, int value)
+{
+    g->values[y][x] = value;
 }
 
 int grid_get_up(int * value, int * newx, int * newy,
@@ -137,11 +145,11 @@ void free_grid(struct grid * g)
 
 void print_grid(const struct grid * g)
 {
-    for(int x = 0; x < g->width; x++)
+    for(int y=0; y < g->height; y++)
     {
-        for(int y=0; y < g->height; y++)
+        for(int x = 0; x < g->width; x++)
         {
-            printf("%d", g->values[y][x]);
+            printf("%x", g->values[y][x]);
         }
         printf("\n");
     }
@@ -196,6 +204,53 @@ int is_low_point(const struct grid * g, int x, int y)
     return 1;
 }
 
+int count_all_connected(struct grid * g, int x, int y)
+{
+    int retval = 0;
+    int newx, newy, newvalue;
+
+    //DBGPRINT("  counting connected (%d,%d)\n", x, y);
+    /* assume this point has been counted already */
+    if(grid_get_up(&newvalue, &newx, &newy, g, x, y))
+    {
+        if(newvalue < 9)
+        {
+            grid_set_value(g, newx, newy, COUNTED);
+            retval += 1;
+            retval += count_all_connected(g, newx, newy);
+        }
+    }
+    if(grid_get_down(&newvalue, &newx, &newy, g, x, y))
+    {
+        if(newvalue < 9)
+        {
+            grid_set_value(g, newx, newy, COUNTED);
+            retval += 1;
+            retval += count_all_connected(g, newx, newy);
+        }
+    }
+    if(grid_get_left(&newvalue, &newx, &newy, g, x, y))
+    {
+        if(newvalue < 9)
+        {
+            grid_set_value(g, newx, newy, COUNTED);
+            retval += 1;
+            retval += count_all_connected(g, newx, newy);
+        }
+    }
+    if(grid_get_right(&newvalue, &newx, &newy, g, x, y))
+    {
+        if(newvalue < 9)
+        {
+            grid_set_value(g, newx, newy, COUNTED);
+            retval += 1;
+            retval += count_all_connected(g, newx, newy);
+        }
+    }
+
+    return retval;
+}
+
 int main(int argc, char ** argv)
 {
     struct grid g;
@@ -215,10 +270,71 @@ int main(int argc, char ** argv)
         }
     }
 
-    free_grid(&g);
-
     printf("Sum of the risk levels of all the low points: %d\n", risk_sum);
 
+    /* now part 2 */
+    int basin_sizes[1000];
+    int basin_idx = 0;
+    for(x=0; x < g.width; x++)
+    {
+        for(y=0; y < g.height; y++)
+        {
+            /* is this point in a basin? */
+            DBGPRINT("Checking for basin at (%d,%d)\n", x, y);
+            if(grid_get_value(&g, x, y) < 9)
+            {
+                /* set this value as counted */
+                grid_set_value(&g, x, y, COUNTED);
+                basin_sizes[basin_idx] = 1;
+
+                /* count all points connected to this one that have value < 9 */
+                DBGPRINT("Found new basin at (%d,%d)\n", x, y);
+                basin_sizes[basin_idx] += count_all_connected(&g, x, y);
+                basin_idx++;
+                print_grid(&g);
+            }
+        }
+    }
+
+    int max_size_basins[3];
+    int max_idx, this_basin_size, save;
+    memcpy(max_size_basins, basin_sizes, sizeof(max_size_basins));
+    DBGPRINT("basin sizes: %d %d %d", basin_sizes[0], basin_sizes[1], basin_sizes[2]);
+    for(int i=sizeof(max_size_basins)/sizeof(max_size_basins[0]); i < basin_idx; i++)
+    {
+        DBGPRINT(" %d", basin_sizes[i]);
+        this_basin_size = basin_sizes[i];
+        for(max_idx = 0;
+                max_idx < sizeof(max_size_basins)/sizeof(max_size_basins[0]);
+                max_idx++)
+        {
+            if(this_basin_size > max_size_basins[max_idx])
+            {
+                /* can't just do a simple replace; the one replaced may be
+                 * larger than one of the other maxes. */
+                save = this_basin_size;
+                this_basin_size = max_size_basins[max_idx];
+                max_size_basins[max_idx] = save;
+            }
+        }
+        DBGPRINT(" (%d %d %d)", max_size_basins[0], max_size_basins[1],
+                max_size_basins[2]);
+    }
+    DBGPRINT("\n");
+
+    int max_product = 1;
+    DBGPRINT("Max size basins: ");
+    for(max_idx = 0; max_idx < sizeof(max_size_basins)/sizeof(max_size_basins[0]); max_idx++)
+    {
+        DBGPRINT("%d ", max_size_basins[max_idx]);
+        max_product *= max_size_basins[max_idx];
+    }
+    DBGPRINT("\n");
+
+    printf("Max size basins: %d\n", max_product);
+    print_grid(&g);
+
+    free_grid(&g);
 
     return 0;
 }

a00b2ab2fd0396f4d382a9ee331a46d83b3347d2
DraftyHat <draftyhat@darken.cybertwilight.org>
2021-12-21 18:09:22 -0500

    day 2 work in progress

--- 2021_21.c
+++ 2021_21.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <unistd.h>
 
-
 #define AOC_YEAR 2021
 #define AOC_DAY 21
 
@@ -15,8 +14,192 @@
 
 #define NPLAYERS 2
 #define NROLLSPERTURN 3
-#define WINNINGSCORE 1000
 #define NPOSITIONS 10
+
+#ifdef TEST
+#define PLAYER1START 4
+#define PLAYER2START 8
+#else
+#define PLAYER1START 1
+#define PLAYER2START 3
+#endif
+
+#ifdef PART2
+
+/* notes on part 2:
+ * - first check that test answers fit in an unsigned long. They do.
+ * possible roll 1, 2, 3
+ * 3 rolls each means possible turn position adds are:
+ *  1 + 1 + 1 = 3
+ *  1 + 1 + 2 = 4
+ *  1 + 1 + 3 = 5
+ *  1 + 2 + 1 = 4
+ *  1 + 2 + 2 = 5
+ *  1 + 2 + 3 = 6
+ *  1 + 3 + 1 = 5
+ *  1 + 3 + 2 = 6
+ *  1 + 3 + 3 = 7
+ *  2 + 1 + 1 = 4
+ *  2 + 1 + 2 = 5
+ *  2 + 1 + 3 = 6
+ *  2 + 2 + 1 = 5
+ *  2 + 2 + 2 = 6
+ *  2 + 2 + 3 = 7
+ *  2 + 3 + 1 = 6
+ *  2 + 3 + 2 = 7
+ *  2 + 3 + 3 = 8
+ *  3 + 1 + 1 = 5
+ *  3 + 1 + 2 = 6
+ *  3 + 1 + 3 = 7
+ *  3 + 2 + 1 = 6
+ *  3 + 2 + 2 = 7
+ *  3 + 2 + 3 = 8
+ *  3 + 3 + 1 = 7
+ *  3 + 3 + 2 = 8
+ *  3 + 3 + 3 = 9
+ *  so we can calculate the position increment after each turn:
+ *     3 in 1 universe
+ *     4 in 3 universes
+ *     5 in 6 universes
+ *     6 in 7 universes
+ *     7 in 6 universes
+ *     8 in 3 universes
+ *     9 in 1 universes
+ * in the universe where a player starts at position 1 and rolls all 1s, it'll
+ * take him 7 turns to win. So games have at most 14 turns. At 3^3 = 27
+ * possible universes per turn, that's (3 ^ 3)^14 = 109418989131512359209
+ * possible universes. So we need to collapse the universes, we can't keep track
+ * of each.
+ * What if we keep track of the relative score? We still need to know when a
+ * player wins, so that doesn't seem like it'll be enough.
+ * There are 21 possible scores in a game, which means 21*21 possible game
+ * scores on any one turn (that's fuzzy, there are actually many fewer, plus a
+ * few because the score may go over 21). So the collection of all scores
+ * should be trackable. But we'd need to track both scores and positions;
+ * that's 21*21*10*10 = 44100 possible positions/scores at any single turn in
+ * the game. That's a lot, but still trackable?
+ */
+
+#define WINNINGSCORE 7
+#define MAXDIEVALUE 3
+
+int main()
+{
+    int player1score, player2score, player1position, player2position;
+    int position_increment, newscore;
+    int turn;
+    int alldone = 0;
+
+    /* number of universes in which a game has a particular score and position,
+     * indexed by [player1 score][player2 score][player1 position][player2
+     * position] */
+    unsigned long allgames[2][WINNINGSCORE][WINNINGSCORE][NPOSITIONS][NPOSITIONS];
+    unsigned long player1wins = 0, player2wins = 0;
+    unsigned long nuniverses;
+    unsigned long new_nuniverses;
+
+    /* number of universes which gain the index number of positions in a single
+     * turn (see breakout above) */
+    int position_increment_per_turn[10] = {
+        0, 0, 0, 1, 3, 6, 7, 6, 3, 1 };
+#define MINIMUM_POSITION_INCREMENT_PER_TURN 3
+#define MAXIMUM_POSITION_INCREMENT_PER_TURN (sizeof(position_increment_per_turn)/sizeof(position_increment_per_turn[0]))
+
+    memset(allgames, 0, sizeof(allgames));
+    allgames[0][0][0][PLAYER1START - 1][PLAYER2START - 1] = 1;
+
+    /* play out all games */
+    while(!alldone)
+    {
+        DBGPRINT("-- starting turn %d\n", turn);
+        /* zero out the spot where we'll keep the scores for the next turn */
+        memset(allgames[(turn + 1) % 2], 0, sizeof(allgames[0]));
+        alldone = 1;
+        /* for each game we're tracking */
+        for(player1score = 0; player1score < WINNINGSCORE; player1score++)
+        {
+            for(player2score = 0; player2score < WINNINGSCORE; player2score++)
+            {
+                for(player1position = 0; player1position < NPOSITIONS;
+                        player1position++)
+                {
+                    for(player2position = 0; player2position < NPOSITIONS;
+                            player2position++)
+                    {
+                        /* any games at this point? */
+                        nuniverses = allgames[turn % 2][player1score][player2score][player1position][player2position];
+                        DBGPRINT("--- p1 %d at %d, p2 %d at %d: %lu universes\n", player1score, player1position, player2score, player2position, nuniverses);
+                        if(0 != nuniverses)
+                        {
+                            /* calculate range of possible games from here */
+                            for(position_increment = MINIMUM_POSITION_INCREMENT_PER_TURN;
+                                    position_increment < MAXIMUM_POSITION_INCREMENT_PER_TURN;
+                                    position_increment++)
+                            {
+                                /* calculate number of new universes that will
+                                 * have this score */
+                                new_nuniverses = position_increment_per_turn[position_increment] * nuniverses;
+                                /* calculate new score */
+                                /* if player 1, add score to player 1, else add
+                                 * to player 2 */
+                                if(turn % 2)
+                                {
+                                    newscore = player1score +
+                                        ((player1position + position_increment) % NPOSITIONS)
+                                        + 1; /* positions index 0-9, position values 1-10 */
+                                    /* mark wins */
+                                    if(newscore >= WINNINGSCORE)
+                                    {
+                                        player1wins += new_nuniverses;
+                                    }
+                                    else
+                                    {
+                                        /* mark games at new score/position */
+                                        allgames[(turn + 1) % 2]
+                                            [(player1position + position_increment) % NPOSITIONS]
+                                            [player2position]
+                                            [newscore]
+                                            [player2score] += new_nuniverses;
+                                        alldone = 0;
+                                    }
+                                }
+                                else /* player 2's turn */
+                                {
+                                    newscore = player2score +
+                                        ((player2position + position_increment) % NPOSITIONS)
+                                        + 1; /* positions index 0-9, position values 1-10 */
+                                    if(newscore >= WINNINGSCORE)
+                                        player2wins += new_nuniverses;
+                                    else
+                                    {
+                                        /* mark games at new score/position */
+                                        allgames[(turn + 1) % 2]
+                                            [player1position]
+                                            [(player2position + position_increment) % NPOSITIONS]
+                                            [player1score]
+                                            [newscore] += new_nuniverses;
+                                        alldone = 0;
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        turn++;
+    }
+
+    printf("Player 1 wins: %lu\n", player1wins);
+    printf("Player 2 wins: %lu\n", player2wins);
+}
+
+
+
+#else /* part 1 */
+
+#define WINNINGSCORE 1000
 
 int main(int argc, char ** argv)
 {
@@ -30,13 +213,8 @@ int main(int argc, char ** argv)
 
     memset(scores, 0, sizeof(scores));
            
-#ifdef TEST
-    positions[0] = 4 - 1;
-    positions[1] = 8 - 1;
-#else
-    positions[0] = 1 - 1;
-    positions[1] = 3 - 1;
-#endif
+    positions[0] = PLAYER1START - 1;
+    positions[1] = PLAYER2START - 1;
 
     /* on each iteration:
      *  increment player index modulo NPLAYERS
@@ -76,3 +254,4 @@ int main(int argc, char ** argv)
 
     return 0;
 }
+#endif
